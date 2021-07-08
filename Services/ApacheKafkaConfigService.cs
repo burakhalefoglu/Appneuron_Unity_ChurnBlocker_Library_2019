9077e486467a4bb045cbf78db06565389b30639a
Burak Halefoğlu <42536490+burakhalefoglu@users.noreply.github.com>
2021-07-08 12:50:09 +0300

    refactoring
    refactoring

---
+++ Services/ApacheKafkaConfigService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appneuron.Services
+{
+    public static class ApacheKafkaConfigService
+    {
+        public const string BootstrapServers = "192.168.1.39:9092";
+
+    }
+}
